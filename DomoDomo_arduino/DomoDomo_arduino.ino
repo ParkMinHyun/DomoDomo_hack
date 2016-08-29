78cb0806f1632a855e880d7961b0d7f12cb50098
박민현 <algusl92@naver.com>
2016-08-30 02:44:48 +0900

    [민현] MP3 출력 기본 셋팅 완료

--- DomoDomo_arduino/DomoDomo_arduino.ino
+++ DomoDomo_arduino/DomoDomo_arduino.ino
@@ -1,9 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
 
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+#define CARDCS 4     // Card chip select pin
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+  
+void setup() {
+  Serial.begin(9600);
+  pinMode(2,INPUT_PULLUP);
+  Serial.println("Adafruit VS1053 Simple Test");
+
+  if (! musicPlayer.begin()) { 
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial.println(F("VS1053 found"));
+  
+  SD.begin(CARDCS);    // initialise the SD card
+  
+  musicPlayer.setVolume(20,20);
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  
+  Serial.println(F("Playing track 002"));
+  musicPlayer.startPlayingFile("002.mp3");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int button1 = digitalRead(2);
+    Serial.println(button1);
+    
+   if (button1 == 0) {
+      if (! musicPlayer.paused()) {
+        Serial.println("Paused");
+        musicPlayer.pausePlaying(true);
+      } else { 
+        Serial.println("Resumed");
+        musicPlayer.pausePlaying(false);
+      }
+    }
+    
+  if (musicPlayer.stopped()) {
+    Serial.println("Done playing music");
+    while (1);
+  }
+  if (Serial.available()) {
+    char c = Serial.read();
+    
+    if (c == 's') {
+      musicPlayer.stopPlaying();
+    }
+    
+    if (c == 'p') {
+      if (! musicPlayer.paused()) {
+        Serial.println("Paused");
+        musicPlayer.pausePlaying(true);
+      } else { 
+        Serial.println("Resumed");
+        musicPlayer.pausePlaying(false);
+      }
+    }
+  }
 
+  delay(100);
 }
