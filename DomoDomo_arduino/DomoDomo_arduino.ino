b42d9c78d44a0336b3876b83ad2db824f0dc0a05
박민현 <algusl92@naver.com>
2016-08-31 01:25:44 +0900

    [민현] 블루투스 통신을 통해 mp3 play 할 수 있도록 함.

--- DomoDomo_arduino/DomoDomo_arduino.ino
+++ DomoDomo_arduino/DomoDomo_arduino.ino
@@ -1,8 +1,9 @@
+#include<SoftwareSerial.h>
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
 #include <SD.h>
 
-unsigned long prevMillis = 0;
+unsigned long prev_LED1_Millis = 0;
 int ledState = LOW;
 
 #define BREAKOUT_RESET  9      // VS1053 reset pin (output)
@@ -19,7 +20,15 @@ int ledState = LOW;
 Adafruit_VS1053_FilePlayer musicPlayer = 
   Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
   
+int blueTx=6;
+int blueRx=5;
+SoftwareSerial mySerial(blueTx,blueRx);
+
+String myString="";
+int bluetooth_serial_num = 99;
+
 void setup() {
+  mySerial.begin(9600);
   Serial.begin(9600);
   pinMode(2,INPUT_PULLUP);
 
@@ -30,18 +39,28 @@ void setup() {
   SD.begin(CARDCS);    // initialise the SD card
   musicPlayer.setVolume(20,20);
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
-  
-  Serial.println(F("Playing track 002"));
-  musicPlayer.startPlayingFile("002.mp3");
 }
-
-void loop() {
-
   
+void loop() {
   int play_stop_button = digitalRead(2);
-  unsigned long currentMillis = millis(); 
-
-  if(currentMillis - prevMillis >= 1000)
+  unsigned long current_LED1_Millis = millis(); 
+  
+  while(mySerial.available())
+  {
+    char myChar = (char)mySerial.read();
+    myString = myString + myChar; 
+    bluetooth_serial_num = myString.toInt();
+    delay(10);
+  }
+  
+  if(!myString.equals(""))
+  {
+     Serial.println("Input value = "+bluetooth_serial_num);
+  }
+  
+  
+  /* LED1 Millis 함수로 구현 */
+  if(current_LED1_Millis - prev_LED1_Millis >= 1000)
   { 
     musicPlayer.GPIO_pinMode(0, OUTPUT);
     if (ledState == LOW){ //꺼져있으면
@@ -51,9 +70,22 @@ void loop() {
     }
      
     musicPlayer.GPIO_digitalWrite(0, ledState);
-    prevMillis = currentMillis;
+    prev_LED1_Millis = current_LED1_Millis;
   }
-  /*
+
+  if(bluetooth_serial_num == 0)
+  {
+     musicPlayer.startPlayingFile("001.mp3");
+  }
+  else if(bluetooth_serial_num == 1)
+  {
+     musicPlayer.startPlayingFile("002.mp3");
+  }
+  else if(bluetooth_serial_num == 2)
+  {
+     musicPlayer.startPlayingFile("002.mp3");
+  }
+  
   if (play_stop_button == 0) 
   {
       if (! musicPlayer.paused()) 
@@ -65,15 +97,13 @@ void loop() {
         musicPlayer.pausePlaying(false);
       }
     }
-    */
+    
+    /*
   if (play_stop_button == 0) 
   {
     musicPlayer.startPlayingFile("001.mp3");
-  }
-  
-  if (musicPlayer.stopped()) {
-    Serial.println("Done playing music");
-    while (1);
-  }
-  
+  }*/
+ 
+  myString="";
+  bluetooth_serial_num = 99;
 }
